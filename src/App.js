bf1c490d0c9277e4cf4c993a2446d57ac2545815
tynchtyk <tynchtyktabyldiev@gmail.com>
2022-05-24 19:19:22 +0600

    react

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className='App'>
+      <a href="">About</a>
+      <a href="">Home</a>
+      <a href="">serviks</a>
+      <a href=""></a>
+
       </header>
-    </div>
+ 
   );
 }
 
